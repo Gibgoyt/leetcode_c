bf62a6a820c983ee733c31d28d4a12231728cf04
Ahmed <ahmedmoti767@gmail.com>
2026-05-29 19:26:12 +0200

    trial 1 works but some malloc issue

--- my_solutions/2/trial_1.c
+++ my_solutions/2/trial_1.c
@@ -10,6 +10,21 @@ typedef struct ListNode {
 	struct ListNode *next;
 } ListNode;
 
+/*
+ *	function declarations
+*/
+static ListNode *push (
+	int val,
+	ListNode *next
+);
+static void print_list (
+	ListNode *head
+);
+ListNode *addTwoNumbers(
+	struct ListNode *l1, 
+	struct ListNode *l2
+);
+
 ListNode *addTwoNumbers(
 	struct ListNode *l1, 
 	struct ListNode *l2
@@ -24,9 +39,32 @@ ListNode *addTwoNumbers(
 	 *	never use a while loop because while loop can infinitely loop, and this edge case needs recompiling
 	 *	as for practise on embedded systems never ever use a while loop
 	 *	this for loop **MUST ALWAYS** have a definite end and must never ever be able to infinitely loop
+	 *
+	 *	every pass over l1 and/or l2 toward NULL, once both l1, l2 are NULL sum is just the carry
+	 *	carry clears and the next test fails, so the loop is bounded by (max(len(l1), len(l2)) + 1)
 	*/
-	for (;;) {
+	for (;
+		NULL != l1 ||
+		NULL != l2 || 
+		0 != carry
+	;) {
 		int sum = carry;
+
+		if (NULL != l1) {
+			sum += l1->val;
+			l1 = l1->next;
+		}
+
+		if (NULL != l2) {
+			sum += l2->val;
+			l2 = l2->next;
+		}
+
+		carry = sum / 10;
+
+		// TODO!!: fix Incompatible integer to pointer conversion assigning to 'struct ListNode *'
+		tail->next = push(sum % 10, NULL);
+		tail = tail->next;
 	}
 	
 	return nil_sentinel.next;
@@ -34,8 +72,8 @@ ListNode *addTwoNumbers(
 
 /*
  *	local test harness
+ * 	TODO!!: please fix Static declaration of 'push' follows non-static declaration
 */
-
 static ListNode *push (
 	int val,
 	ListNode *next
@@ -57,11 +95,13 @@ static void print_list (
 	 *	again we gotta try avoid the while(HEAD != NULL) and use a for loop
 	 *	but again, no point of an infinite for loop that will be the same
 	*/
-	for (;;) {
+	for (; 
+		NULL != head; 
+		head = head->next
+	) {
 		printf("%d", head->val);
 		if (NULL != head->next) {
 			printf(" -> ");
-			head = head->next;
 		}
 		printf("\n");
 	}
