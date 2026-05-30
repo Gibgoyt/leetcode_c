7d4dead94d4ddcee8d34d9228cfe4c0035f796ec
Ahmed <ahmedmoti767@gmail.com>
2026-05-30 09:13:20 +0200

    trial 1, buffer lookup table created

--- my_solutions/1/trial_1.c
+++ my_solutions/1/trial_1.c
@@ -1,12 +1,73 @@
 #include <stdbool.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ *	function declarations because we are fucking awasome :(//
+*/
+bool hasDuplicate(
+	int *nums,
+	int numsSize
+);
+uint8_t test_harness();
 
 bool hasDuplicate(
-	int* nums, 
+	int *nums, 
 	int numsSize
 ) {
-	return 0;
+	/*
+	 *	my initial attempt would go about having a lookup table, storing the list there
+	 *	I can just iterate i, j through nums[], but ofc that is a bit silly
+	 *	so we rather have a lookup table, store nums[0] at seen[0], then move to nums[1], if not seen[0] then store nums[0] at seen[0]
+	 *	then move to nums[2], check seen[0] and seen[1] if match
+	 *	this is still a O(n^2) I think
+	 *
+	 *	buffer becomes tricky because we are going to have to do a bounds check!
+	 *	this is why I need help
+	 *
+	 *	buffer of every number seen so far
+	 *	max size is numsSize in the all unique case
+	*/
+	int *seen = malloc((size_t)numsSize * sizeof(int));
+
+	int seenCount = 0;
+	bool ret;
+
+	for (
+		uint32_t i=0; 
+		i<numsSize;
+		i++
+	) {
+		// scan only what we have actually stored
+		for (
+			uint32_t j=0;
+			j<seenCount; 
+			j++
+		) {
+			if (nums[i] == seen[j]) {
+				ret = true;
+				goto cleanup_and_exit;
+			}
+		}
+		// never seen this value, then append if
+		seen[seenCount] = nums[i];
+		seenCount++;
+	}
+	// we have reach nums[-1] nicely, therefore no duplicate
+	ret = false;
+	cleanup_and_exit:
+		free(seen);
+		return ret;
 }
 
 int main () {
+	int a[] = {1, 2, 3, 3};
+	int b[] = {1, 2, 3, 4};
+
+	printf("[1, 2, 3, 3] -> %d (expected 1)\n", hasDuplicate(a, 4));
+	printf("[1, 2, 3, 4] -> %d (expected 0)\n", hasDuplicate(b, 4));
+
 	return 0;
 }
