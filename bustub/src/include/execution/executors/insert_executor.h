e9627ff2a8de9b6ea32030d94d33f0cd77e6fa89
Ahmed Moti <ahmedmoti767@gmail.com>
2026-06-01 12:27:58 +0200

    docs added for insert_executor

--- bustub/src/include/execution/executors/insert_executor.h
+++ bustub/src/include/execution/executors/insert_executor.h
@@ -21,27 +21,48 @@
 #include "storage/table/tuple.h"
 
 namespace bustub {
-
-/**
- * InsertExecutor executes an insert on a table.
- * Inserted values are always pulled from a child executor.
- */
-class InsertExecutor : public AbstractExecutor {
- public:
-  InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
-                 std::unique_ptr<AbstractExecutor> &&child_executor);
-
-  void Init() override;
-
-  auto Next(std::vector<bustub::Tuple> *tuple_batch, std::vector<bustub::RID> *rid_batch, size_t batch_size)
-      -> bool override;
-
-  /** @return The output schema for the insert */
-  auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
-
- private:
-  /** The insert plan node to be executed*/
-  const InsertPlanNode *plan_;
-};
-
+	/*
+	 *	InsertExecutor
+	 *		DML executor for insert
+	 *
+	 *	pulls tuples from a child executor + writes into target table heap
+	 *	in project 4 we must also:
+	 *		- stamp inserted base tuple with the current txn's "temp ts"
+	 *		  (i.e. the txnId, so that other txns can recognize uncommitted writes)
+	 *		- record the RID in the txn's write_set_ so commit/abort can later stamp/undo change
+	 *
+	 *	Next() returns the count (i.e. no. of inserted rows) wrapped in a single tuple, exactly once
+	 *	subsequent calls return false
+	*/
+	
+	/*
+	 * InsertExecutor executes an insert on a table.
+	 * Inserted values are always pulled from a child executor.
+	*/
+	class InsertExecutor : public AbstractExecutor {
+		public:
+			InsertExecutor(
+				ExecutorContext *exec_ctx, 
+				const InsertPlanNode *plan,
+			        std::unique_ptr<AbstractExecutor> &&child_executor
+			);
+	
+			void Init() override;
+	
+			auto Next(
+				std::vector<bustub::Tuple> *tuple_batch, 
+				std::vector<bustub::RID> *rid_batch, 
+				size_t batch_size
+			) -> bool override;
+	
+			/** @return The output schema for the insert */
+			auto GetOutputSchema() const -> const Schema & override { 
+				return plan_->OutputSchema(); 
+			};
+	
+		private:
+			/** The insert plan node to be executed*/
+			const InsertPlanNode *plan_;
+	};
+	
 }  // namespace bustub
