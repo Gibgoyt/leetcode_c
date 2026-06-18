739bfaef0d6be2a3bd1ad34cbd94d418fd33abe6
Ahmed <ahmedmoti767@gmail.com>
2026-06-18 19:20:17 +0200

    proper print program

--- lessons/1/main.cpp
+++ lessons/1/main.cpp
@@ -13,5 +13,25 @@ int main () {
 		std::println("hello, world");
 	#endif
 	std::cout << "Hello World" << std::endl;
+
+	std::uint32_t count = 42;
+	std::uint64_t big = 1'000'000'000ULL;
+	std::size_t length = 0;
+
+	/*
+	 *	RAII containers
+	 *	owns its own memory
+	 *	and frees automatically
+	*/
+	std::string name = "Ahmed";
+	std::vector<std::uint32_t> xs = {1, 2, 3, 4, 5};
+	length = xs.size();
+
+	#if defined(PRINT)
+		std::println("name={}, count={}, big={}, length={}", name, count, big, length);
+	#endif
+
+	std::cout << "name=" << name << ", count=" << count << ", big=" << big << ", length=" << length << "\n";
+
 	return 0;
 }
