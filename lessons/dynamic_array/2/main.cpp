0a024a878b8e093f44bec2b7999e8a24a35015e7
Ahmed <ahmedmoti767@gmail.com>
2026-06-20 17:09:55 +0200

    our own implementation of std::vector class started

---
+++ lessons/dynamic_array/2/main.cpp
@@ -0,0 +1,10 @@
+/*
+ *	here we will try create our own dynamic heap contiguous buffer class
+ *	similar to std::vector
+*/
+
+#include <iostream>
+
+int main () {
+	std::cout << "Hello World!\n";
+}
